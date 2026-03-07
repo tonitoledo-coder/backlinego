69bbdd00a0a955cafea37f207112daab2f25d69b
Toni Toledo <tonitoledo@gmail.com>
2026-03-07 10:57:41 +0000

    File changes

--- src/pages/EquipmentDetail.jsx
+++ src/pages/EquipmentDetail.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { createPageUrl } from '@/utils';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
